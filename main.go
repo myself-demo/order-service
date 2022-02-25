21889cbdda214b4867d4a67f53b231dfec5cf2b0
Husky <husky.robot.dog@gmail.com>
2022-02-26 01:43:04 +0800

    init

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/myself-demo/order-service-api/pb"
+	"github.com/myself-demo/order-service/service"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:9000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	server := grpc.NewServer()
+	pb.RegisterOrderServiceServer(server, service.NewOrderServiceProvider())
+	if err := server.Serve(lis); err != nil {
+		log.Fatalln(err)
+	}
 }
